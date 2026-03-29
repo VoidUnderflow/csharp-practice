186046e7c3a04cf8594ddbc5a55df538cda45307
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-03-29 12:52:34 +0300

    Wire in root route.
    ;)

--- EventsApp/client/src/main.tsx
+++ EventsApp/client/src/main.tsx
@@ -1,19 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import App from "./app/layout/App.tsx";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router";
+import { router } from "./app/routes/Routes.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 );
