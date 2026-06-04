b8f2580013d25d6f65a8369a9f2a4f26192662d8
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-06-04 15:10:12 +0100

    Add some basic form validation with Zod.

--- EventsApp/client/src/features/activities/form/ActivityForm.tsx
+++ EventsApp/client/src/features/activities/form/ActivityForm.tsx
@@ -3,9 +3,22 @@ import { useEffect } from "react";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import { useParams } from "react-router";
 import { useForm, type FieldValues } from "react-hook-form";
+import {
+  activitySchema,
+  type ActivitySchema,
+} from "../../../lib/schemas/activitySchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function ActivityForm() {
-  const { register, reset, handleSubmit } = useForm();
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ActivitySchema>({
+    mode: "onTouched",
+    resolver: zodResolver(activitySchema),
+  });
   const { id } = useParams();
   const { updateActivity, createActivity, activity, isLoadingActivity } =
     useActivities(id);
@@ -37,6 +50,8 @@ export default function ActivityForm() {
           name="title"
           label="Title"
           defaultValue={activity?.title}
+          error={!!errors.title}
+          helperText={errors.title?.message}
         />
         <TextField
           {...register("description")}
