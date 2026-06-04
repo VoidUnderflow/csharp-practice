5b4522dd1a3bab0d48050ab07f63e9d9df6c408c
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-06-04 20:21:57 +0100

    Add Date validation.

--- EventsApp/client/src/lib/schemas/activitySchema.ts
+++ EventsApp/client/src/lib/schemas/activitySchema.ts
@@ -9,7 +9,11 @@ export const activitySchema = z.object({
   title: requiredString("Title"),
   description: requiredString("Description"),
   category: requiredString("Category"),
-  date: z.date({ error: "Date is required" }),
+  date: z
+    .date({ error: "Date is required" })
+    .refine((date) => date > new Date(Date.now() + 30 * 60 * 1000), {
+      message: "Date must be at least 30 minutes from now.",
+    }),
   city: requiredString("City"),
   venue: requiredString("Venue"),
 });
