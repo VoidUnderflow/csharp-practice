dec483dfa9c99e7a0e71c4e89406585ed7dc19c8
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-06-04 12:03:06 +0100

    Add dummy error-triggering page and a few routes for different errors.

--- EventsApp/client/src/lib/api/agent.ts
+++ EventsApp/client/src/lib/api/agent.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { store } from "../stores/store";
+import { toast } from "react-toastify";
+import { router } from "../../app/routes/Routes";
 
 // Introduces an artificial delay when fetching things, for illustrative purposes.
 
@@ -16,14 +18,51 @@ agent.interceptors.response.use((config) => {
   return config;
 });
 
-agent.interceptors.response.use(async (response) => {
-  try {
+agent.interceptors.response.use(
+  async (response) => {
+    // The delay mentioned above.
     await sleep(1000);
-    return response;
-  } catch (error) {
-    console.log(error);
-    return Promise.reject(error);
-  } finally {
+
     store.uiStore.isIdle();
-  }
-});
+    return response;
+  },
+
+  async (error) => {
+    await sleep(1000);
+
+    store.uiStore.isIdle();
+
+    const { status, data } = error.response;
+    switch (status) {
+      case 400:
+        if (data.errors) {
+          // Validation error.
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
+            }
+          }
+
+          throw modalStateErrors.flat();
+        } else {
+          // Any other 400 error.
+          toast.error(data);
+        }
+        break;
+      case 401:
+        toast.error("Unauthorised");
+        break;
+      case 404:
+        router.navigate("/not-found");
+        break;
+      case 500:
+        router.navigate("/server-error", { state: { error: data } });
+        break;
+      default:
+        break;
+    }
+
+    return Promise.reject(error);
+  },
+);
