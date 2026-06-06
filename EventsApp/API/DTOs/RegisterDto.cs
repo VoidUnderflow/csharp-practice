fcf3ad11df3576e0811a69bad8c9fea8016b2a32
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-06-06 18:07:45 +0100

    Add endpoint for user registration.

---
+++ EventsApp/API/DTOs/RegisterDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    public string DisplayName { get; set; } = "";
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = "";
+
+    // Identity already enforces this to be required + complex enough.
+    public string Password { get; set; } = "";
+}
