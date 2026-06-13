96ad63a59f15031b58481d4f93a575610f30a888
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-06-13 15:36:28 +0100

    Update test seed data.

--- EventsApp/Persistence/DbInitialiser.cs
+++ EventsApp/Persistence/DbInitialiser.cs
@@ -7,16 +7,17 @@ public class DbInitialiser
 {
     public static async Task SeedData(AppDbContext context, UserManager<User> userManager)
     {
+
+        var users = new List<User>
+        {
+            new() {DisplayName="Bob", UserName="bob@test.com", Email="bob@test.com"},
+            new() {DisplayName="Jane", UserName="jane@test.com", Email="jane@test.com"},
+            new() {DisplayName="Tom", UserName="tom@test.com", Email="tom@test.com"},
+        };
+
         // Seed test users.
         if (!userManager.Users.Any())
         {
-            var users = new List<User>
-            {
-                new() {DisplayName="Bob", UserName="bob@test.com", Email="bob@test.com"},
-                new() {DisplayName="Jane", UserName="jane@test.com", Email="jane@test.com"},
-                new() {DisplayName="Tom", UserName="tom@test.com", Email="tom@test.com"},
-            };
-
             foreach (var user in users)
             {
                 await userManager.CreateAsync(user, "Pa$$w0rd");
@@ -24,7 +25,6 @@ public class DbInitialiser
         }
 
         if (context.Activities.Any()) return;
-
         var activities = new List<Activity>
         {
             new()
@@ -38,6 +38,19 @@ public class DbInitialiser
                     "The Lamb and Flag, 33, Rose Street, Seven Dials, Covent Garden, London, Greater London, England, WC2E 9EB, United Kingdom",
                 Latitude = 51.51171665,
                 Longitude = -0.1256611057818921,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[0].Id,
+                        IsHost = true,
+                    },
+                    new()
+                    {
+                        UserId = users[1].Id,
+                        IsHost = false,
+                    }
+                ]
             },
             new()
             {
@@ -49,7 +62,23 @@ public class DbInitialiser
                 Venue =
                     "Louvre Museum, Rue Saint-Honoré, Quartier du Palais Royal, 1st Arrondissement, Paris, Ile-de-France, Metropolitan France, 75001, France",
                 Latitude = 48.8611473,
-                Longitude = 2.33802768704666
+                Longitude = 2.33802768704666,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[1].Id,
+                        IsHost = true,
+                    },
+                    new()
+                    {
+                        UserId = users[2].Id
+                    },
+                    new()
+                    {
+                        UserId = users[0].Id,
+                    }
+                ]
             },
             new()
             {
@@ -60,7 +89,15 @@ public class DbInitialiser
                 City = "London",
                 Venue = "Natural History Museum",
                 Latitude = 51.496510900000004,
-                Longitude = -0.17600190725447445
+                Longitude = -0.17600190725447445,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[2].Id,
+                        IsHost = true,
+                    }
+                ]
             },
             new()
             {
@@ -71,7 +108,19 @@ public class DbInitialiser
                 City = "London",
                 Venue = "The O2",
                 Latitude = 51.502936649999995,
-                Longitude = 0.0032029278126681844
+                Longitude = 0.0032029278126681844,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[0].Id,
+                        IsHost = true,
+                    },
+                    new()
+                    {
+                        UserId = users[2].Id
+                    }
+                ]
             },
             new()
             {
@@ -82,7 +131,15 @@ public class DbInitialiser
                 City = "London",
                 Venue = "The Mayflower",
                 Latitude = 51.501778,
-                Longitude = -0.053577
+                Longitude = -0.053577,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[1].Id,
+                        IsHost = true,
+                    }
+                ]
             },
             new()
             {
@@ -93,7 +150,19 @@ public class DbInitialiser
                 City = "London",
                 Venue = "The Blackfriar",
                 Latitude = 51.512146650000005,
-                Longitude = -0.10364680647106028
+                Longitude = -0.10364680647106028,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[2].Id,
+                        IsHost = true,
+                    },
+                    new()
+                    {
+                        UserId = users[0].Id
+                    }
+                ]
             },
             new()
             {
@@ -105,7 +174,15 @@ public class DbInitialiser
                 Venue =
                     "Sherlock Holmes Museum, 221b, Baker Street, Marylebone, London, Greater London, England, NW1 6XE, United Kingdom",
                 Latitude = 51.5237629,
-                Longitude = -0.1584743
+                Longitude = -0.1584743,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[0].Id,
+                        IsHost = true,
+                    }
+                ]
             },
             new()
             {
@@ -117,18 +194,42 @@ public class DbInitialiser
                 Venue =
                     "Roundhouse, Chalk Farm Road, Maitland Park, Chalk Farm, London Borough of Camden, London, Greater London, England, NW1 8EH, United Kingdom",
                 Latitude = 51.5432505,
-                Longitude = -0.15197608174931165
+                Longitude = -0.15197608174931165,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[1].Id,
+                        IsHost = true,
+                    },
+                    new()
+                    {
+                        UserId = users[0].Id
+                    }
+                ]
             },
             new()
             {
                 Title = "Future Activity 7",
                 Date = DateTime.Now.AddMonths(7),
-                Description = "Activity 2 months ago",
+                Description = "Activity 7 months in future",
                 Category = "travel",
                 City = "London",
                 Venue = "River Thames, England, United Kingdom",
                 Latitude = 51.5575525,
-                Longitude = -0.781404
+                Longitude = -0.781404,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[2].Id,
+                        IsHost = true,
+                    },
+                    new()
+                    {
+                        UserId = users[1].Id
+                    }
+                ]
             },
             new()
             {
@@ -137,9 +238,17 @@ public class DbInitialiser
                 Description = "Activity 8 months in future",
                 Category = "film",
                 City = "London",
-                Venue = "River Thames, England, United Kingdom",
+                Venue = "Odeon Leicester Square",
                 Latitude = 51.5575525,
-                Longitude = -0.781404
+                Longitude = -0.781404,
+                Attendees =
+                [
+                    new()
+                    {
+                        UserId = users[0].Id,
+                        IsHost = true,
+                    }
+                ]
             }
         };
 
