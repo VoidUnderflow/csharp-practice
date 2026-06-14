20b119003570c9304ad4985620cff5bca67289ca
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-06-15 00:41:40 +0100

    Add Photo entity, map its relationship to User and perform migration.

--- EventsApp/Domain/User.cs
+++ EventsApp/Domain/User.cs
@@ -11,4 +11,6 @@ public class User : IdentityUser
 
     // n-to-n
     public ICollection<ActivityAttendee> Activities { get; set; } = [];
+
+    public ICollection<Photo> Photos { get; set; } = [];
 }
