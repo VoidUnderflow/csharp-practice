b1f3f54056be528e7fc82c59f37bf7607bb4336e
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-06-22 20:40:48 +0100

    Add outline for photo upload widget.

---
+++ EventsApp/client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -0,0 +1,23 @@
+import { Grid, Typography } from "@mui/material";
+
+export default function PhotoUploadWidget() {
+  return (
+    <Grid container spacing={3}>
+      <Grid size={4}>
+        <Typography variant="overline" color="secondary">
+          Step 1 - Add photo
+        </Typography>
+      </Grid>
+      <Grid size={4}>
+        <Typography variant="overline" color="secondary">
+          Step 2 - Resize image
+        </Typography>
+      </Grid>
+      <Grid size={4}>
+        <Typography variant="overline" color="secondary">
+          Step 3 - Preview & upload
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
