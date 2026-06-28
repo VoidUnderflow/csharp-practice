b5c18ccf53450e4100b062345bc8f2b570ffde0c
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-06-28 14:54:27 +0100

    Create store hook for fetching comments for an activity.

---
+++ EventsApp/client/src/lib/hooks/useComments.ts
@@ -0,0 +1,65 @@
+import { useLocalObservable } from "mobx-react-lite";
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  HubConnectionState,
+} from "@microsoft/signalr";
+import { useEffect, useRef } from "react";
+import type { ChatComment } from "../types";
+
+export default function useComments(activityId?: string) {
+  // Workaround strict mode creating two connections.
+  const created = useRef(false);
+
+  const commentStore = useLocalObservable(() => ({
+    hubConnection: null as HubConnection | null,
+    comments: [] as ChatComment[],
+
+    createHubConnection(activityId: string) {
+      if (!activityId) return;
+
+      this.hubConnection = new HubConnectionBuilder()
+        .withUrl(`${import.meta.env.VITE_COMMENT_URL}?=${activityId}`, {
+          withCredentials: true,
+        })
+        .withAutomaticReconnect()
+        .build();
+
+      this.hubConnection
+        .start()
+        .catch((error) =>
+          console.log("Error establishing connection: ", error),
+        );
+
+      this.hubConnection.on("LoadComments", (comments) => {
+        this.comments = comments;
+      });
+
+      this.hubConnection.on("ReceiveComment", (comment) => {
+        this.comments.unshift(comment);
+      });
+    },
+
+    stopHubConnection() {
+      if (this.hubConnection?.state === HubConnectionState.Connected) {
+        this.hubConnection
+          .stop()
+          .catch((error) => console.log("Error stopping connection: ", error));
+      }
+    },
+  }));
+
+  useEffect(() => {
+    if (activityId && !created.current) {
+      commentStore.createHubConnection(activityId);
+      created.current = true;
+    }
+
+    return () => {
+      commentStore.stopHubConnection();
+      commentStore.comments = [];
+    };
+  }, [activityId, commentStore]);
+
+  return { commentStore };
+}
