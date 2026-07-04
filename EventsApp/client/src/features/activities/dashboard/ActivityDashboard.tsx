ea53d44439509ba0bfa794ce6834a07910b7d49a
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-07-04 21:51:02 +0100

    Make filter controls sticky.

--- EventsApp/client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ EventsApp/client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -19,7 +19,10 @@ export function ActivityDashboard() {
           Load more
         </Button>
       </Grid>
-      <Grid size={4}>
+      <Grid
+        size={4}
+        sx={{ position: "sticky", top: 112, alignSelf: "flex-start" }}
+      >
         <ActivityFilters />
       </Grid>
     </Grid>
