1efe2985bfbfbf7ee108d4f1c5968d25bf28e041
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-07-04 22:02:21 +0100

    Add infinite scroll.

--- EventsApp/client/src/features/activities/dashboard/ActivityList.tsx
+++ EventsApp/client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,10 +1,21 @@
 import { Box, Typography } from "@mui/material";
 import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { Fragment } from "react/jsx-runtime";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export function ActivityList() {
-  const { activitiesGroup, isLoading } = useActivities();
+  const { activitiesGroup, isLoading, hasNextPage, fetchNextPage } =
+    useActivities();
+  const { ref, inView } = useInView({
+    threshold: 0.5, // trigger when we are at 1/2 of the element containing the ref
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, hasNextPage, inView]);
 
   if (isLoading) return <Typography>Loading...</Typography>;
   if (!activitiesGroup) return <Typography>No activities found</Typography>;
@@ -12,11 +23,17 @@ export function ActivityList() {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       {activitiesGroup.pages.map((activities, index) => (
-        <Fragment key={index}>
+        <Box
+          key={index}
+          ref={index === activitiesGroup.pages.length - 1 ? ref : null}
+          display={"flex"}
+          flexDirection={"column"}
+          gap={3}
+        >
           {activities.items.map((activity) => (
             <ActivityCard key={activity.id} activity={activity} />
           ))}
-        </Fragment>
+        </Box>
       ))}
     </Box>
   );
